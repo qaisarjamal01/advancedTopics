2c8e591bb89ee81150d6d885e93affd4d9c46ba4
qaisarjamal01 <qaisarjamaluop@gmail.com>
2025-03-08 23:45:32 +0500

    updates

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,19 +8,37 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  TextEditingController nameEditingController = TextEditingController();
+  String value = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: TextField(
-          decoration: InputDecoration.collapsed(hintText: 'Enter your name'),
-          onChanged: (String text){
-            print('text $text');
-          },
-          onSubmitted: (String text){
-            print('text $text');
-          },
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: TextField(
+              controller: nameEditingController,
+              //here collapsed means it has no default borders
+              decoration: InputDecoration.collapsed(hintText: 'Enter your name'),
+              onChanged: (String text){
+                //here showing every text when i typed
+                setState(() {
+                  value = text;
+                });
+              },
+              onSubmitted: (String text){
+                //print when click on done or ok button or check button
+                setState(() {
+                  value = text;
+                });
+                nameEditingController.clear();
+              },
+            ),
+          ),
+          Text('\n\n $value')
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:advanced_topics/form_validation_learning.dart';
 import 'package:flutter/material.dart';
 import 'home_screen.dart';
 
@@ -12,11 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const HomeScreen()
+      home: const FormValidationLearning()
     );
   }
 }
