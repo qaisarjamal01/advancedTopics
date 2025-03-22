5882f6f6e9bb6cdff8c8033f0f20e4553f6f9e8e
qaisarjamal01 <qaisarjamaluop@gmail.com>
2025-03-22 23:41:38 +0500

    lottie animations

--- lib/animations/lottie_animations.dart
+++ lib/animations/lottie_animations.dart
@@ -8,7 +8,27 @@ class LottieAnimations extends StatefulWidget {
   State<LottieAnimations> createState() => _LottieAnimationsState();
 }
 
-class _LottieAnimationsState extends State<LottieAnimations> {
+class _LottieAnimationsState extends State<LottieAnimations> with SingleTickerProviderStateMixin{
+
+  late AnimationController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(vsync: this);
+    
+    controller.addStatusListener((status) async{
+      if(status == AnimationStatus.completed){
+        Navigator.pop(context);
+        controller.reset();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,11 +37,46 @@ class _LottieAnimationsState extends State<LottieAnimations> {
         centerTitle: true,
         backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: Lottie.asset(
-          'assets/welcome.animations.json',
-        ),
+      body: Column(
+        children: [
+          Center(
+            child: Lottie.asset(
+              'assets/delivery animation.json',
+            ),
+          ),
+          SizedBox(height: 32,),
+          ElevatedButton.icon(
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.symmetric(horizontal: 43),
+              textStyle: TextStyle(fontSize: 38,color: Colors.blue)
+            ),
+            icon: Icon(Icons.delivery_dining,size: 40,),
+              onPressed: showDoneDialog, 
+              label: Text('Order Pizza')
+          )
+        ],
       ),
     );
   }
+
+  void showDoneDialog() => showDialog(
+    barrierDismissible: false,
+    context: context,
+    builder: (context) => Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Lottie.asset('assets/done animation.json',repeat: false,controller: controller,
+          onLoaded: (composition){
+            controller.duration = composition.duration;
+            controller.forward();
+          }
+          ),
+          Text('Enjoy your order',style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),),
+          SizedBox(height: 16,)
+        ],
+      ),
+    )
+  );
 }
